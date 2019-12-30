7764464b0695aac6a43e892ddabb1f6ff63de0aa
Rails Udemy Course <enginbulut@live.com>
2019-12-30 03:11:55 +0300

    add initial migration

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Banking.Data.Context;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDBContext _ctx;
+        public AccountRepository(BankingDBContext ctx)
+        {
+            this._ctx = ctx;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _ctx.Accounts;
+        }
+    }
+}
