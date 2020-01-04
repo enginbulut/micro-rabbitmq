ab8be600b8162fa686aad07d8c981c68b55d70c1
Engin Bulut <enginbulut@live.com>
2020-01-04 16:00:49 +0300

    implementing publish and sendCommand for Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -4,19 +4,34 @@ using System.Collections.Generic;
 using System.Text;
 using MicroRabbit.Banking.Domain.Models;
 using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Domain.Core.Bus;
 
 namespace MicroRabbit.Banking.Application.Services
 {
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
-        public AccountService(IAccountRepository accountRepository)
+        private readonly IEventBus _bus;
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             this._accountRepository = accountRepository;
+            this._bus = bus;
         }
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                    accountTransfer.FromAccount,
+                    accountTransfer.ToAccount,
+                    accountTransfer.TransferAmount
+                );
+            _bus.SendCommand(createTransferCommand);
         }
     }
 }
